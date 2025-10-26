6e74802b1cc791e318f116f4faea64d3d3e62855
yverma27 <yverma@uw.edu>
2025-10-26 16:00:10 -0700

    created variables and setup for sketch 2

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,7 +1,21 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  let totalPages = 80;
+  let leftCount = 0;
+  let rightCount = totalPages;
+  let pageThickness = 2.2; // visual thickness per page in px
+  let gap = 120; // gap between left and right stacks
+  let lastFlipTime = 0;
+  let flipping = false;
+  let flipStart = 0;
+  let flipDuration = 420; // ms for a single page flip animation
+  let flipPageIndex = 0;
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textFont('Helvetica');
+    lastFlipTime = p.millis();
+
   };
   p.draw = function () {
     p.background(240, 200, 200);
