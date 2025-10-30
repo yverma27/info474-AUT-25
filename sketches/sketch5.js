02a9ed85c4bcb183251b83d97803351cc258327f
yverma27 <yverma@uw.edu>
2025-10-30 12:06:43 -0700

    added function for process data and get section 1 bar chart data

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -13,7 +13,40 @@ registerSketch('sk5', function (p) {
 
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textFont('Arial');
+    p.noLoop();
+    colorScale = colorGradient("#ff4b5c", "#4caf50", 10);
+    processData();
   };
+
+  function processData() {
+    let academicImpact = table.getColumn("Affects_Academic_Performance");
+    yesCount = academicImpact.filter(v => v.toLowerCase() === "yes").length;
+    noCount = academicImpact.filter(v => v.toLowerCase() === "no").length;
+
+    // Section 1 - Bar Chart Data
+    let addictionBins = {};
+    for (let r of table.rows) {
+      let score = parseInt(r.get("Addicted_Score"));
+      let sleep = parseFloat(r.get("Sleep_Hours_Per_Night"));
+      if (!isNaN(score) && !isNaN(sleep)) {
+        if (!addictionBins[score]) addictionBins[score] = [];
+        addictionBins[score].push(sleep);
+      }
+    }
+
+    for (let score in addictionBins) {
+      let avgSleeps = addictionBins[score].reduce((a, b) => a + b, 0) / addictionBins[score].length;
+      avgSleepByAddiction.push({ score: parseInt(score), sleep: avgSleeps });
+    }
+    avgSleepByAddiction.sort((a, b) => a.score - b.score);
+
+
+
+
+
+
+  }
 
   p.draw = function () {
 
